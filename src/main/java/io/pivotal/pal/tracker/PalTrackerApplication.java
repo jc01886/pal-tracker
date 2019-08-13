1e30b2b9bf37315537aa4f1eb2d633e8e7c888d6
rmpatel101 <rmpatel101@gmail.com>
2019-08-13 16:11:10 -0400

    jdbc template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
+
 @SpringBootApplication
 public class PalTrackerApplication {
     public static void main(String[] args) {
@@ -11,8 +14,8 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository getTimeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository getTimeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
 }
